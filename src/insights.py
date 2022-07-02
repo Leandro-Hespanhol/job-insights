d4d65f3f8d961cde831056080395bb4649605a64
Leandro Hespanhol <leandrohespanhol@gmail.com>
2022-07-02 13:00:48 -0300

    salary range

--- src/insights.py
+++ src/insights.py
@@ -164,7 +164,15 @@ def matches_salary_range(job, salary):
         If `job["min_salary"]` is greather than `job["max_salary"]`
         If `salary` isn't a valid integer
     """
-    pass
+    if "min_salary" not in job or "max_salary" not in job:
+        raise ValueError("job must have a min_salary and max_salary")
+    if type(job["min_salary"]) != int or type(job["max_salary"]) != int:
+        raise ValueError("min_salary and max_salary must be valid integers")
+    if type(salary) != int:
+        raise ValueError("salary must be a valid integer")
+    if job["min_salary"] > job["max_salary"]:
+        raise ValueError("min_salary cannot be greater than max_salary")
+    return salary >= job["min_salary"] and salary <= job["max_salary"]
 
 
 def filter_by_salary_range(jobs, salary):
