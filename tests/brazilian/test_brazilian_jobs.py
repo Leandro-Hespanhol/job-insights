8dd1f3c466014fc59c590512698fb543dd8c5d45
Leandro Hespanhol <leandrohespanhol@gmail.com>
2022-07-05 17:17:05 -0300

    brazilian jobs

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,10 @@
-# from src.brazilian_jobs import read_brazilian_file
+from pickle import FALSE
+from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    braz = read_brazilian_file('tests/mocks/brazilians_jobs.csv')
+    print('braz', braz[0])
+    print('braz222', braz[0].get('titulo'))
+    # assert braz[0].get('titulo') == 'Maquinista'
+    assert braz[0].get('title', 'key not found') == 'Maquinista'
